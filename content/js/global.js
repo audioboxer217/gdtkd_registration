7ebea8e5cdf20173dafc2dc4d9a1c09340543081
Scott Eppler <seppler@presidio.com>
2023-05-23 12:03:19 -0500

    Add formatting and validation for Phone number

--- content/js/global.js
+++ content/js/global.js
@@ -1,3 +1,18 @@
+function formatPhoneNumber(input) {
+  // Remove any non-digits from the input
+  var phoneNum = input.replace(/[^0-9]/g, "");
+
+  // Check if the input is a valid phone number
+  if (phoneNum.length !== 10) {
+    return;
+  }
+
+  // Format the phone number
+  phoneNumFormatted = phoneNum.substring(0, 3) + "-" + phoneNum.substring(3, 6) + "-" + phoneNum.substring(6);
+
+  document.getElementById("inputPhone").value = phoneNumFormatted;
+}
+
 function updateCostDetails() {
   var blackBelt = "The first event for Black Belts is $100 and each additional event is $25"
   var colorBelt = "The first event for Color Belts is $90 and each additional event is $20"
