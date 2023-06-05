2320ce4f07c03355313d3c16f0b04d2f71727237
Scott Eppler <seppler@presidio.com>
2023-06-05 16:51:42 -0500

    Initial work of Flask-based form processing
    relates to Form Processing #4

--- app.py
+++ app.py
@@ -6,13 +6,29 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def handle_form():
     if request.method == "POST":
-        # Form was submitted, process the data
-        name = request.form.get("name")
-        email = request.form.get("email")
-        # ... process the form data as needed
+        form_data = dict(
+            fname=request.form.get("fname"),
+            lname=request.form.get("lname"),
+            email=request.form.get("email"),
+            phone=request.form.get("phone"),
+            address1=request.form.get("address1"),
+            address2=request.form.get("address2"),
+            city=request.form.get("city"),
+            state=request.form.get("state"),
+            zip=request.form.get("zip"),
+            birthdate=request.form.get("birthdate"),
+            age=request.form.get("age"),
+            gender=request.form.get("gender"),
+            weight=request.form.get("weight"),
+            profilePic=request.form.get("profilePic"),
+            school=request.form.get("school"),
+            coach=request.form.get("coach"),
+            beltRank=request.form.get("beltRank"),
+            events=request.form.get("eventList"),
+        )
 
         # Return a response or redirect to another page
-        return "Registration submitted successfully!"
+        return render_template("success.html", form_data=form_data)
     else:
         # Display the form
         return render_template("form.html")
