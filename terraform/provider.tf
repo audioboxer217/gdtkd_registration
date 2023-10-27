66ef342929ab32f9379f6f5eefd6db50b9aac313
Scott Eppler <seppler@presidio.com>
2023-10-27 11:43:18 -0500

    Add TF Cloud details to provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,14 @@
 terraform {
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "OKTKD"
+    workspaces {
+      tags = [
+        "tkd_reg",
+        "infra"
+      ]
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
