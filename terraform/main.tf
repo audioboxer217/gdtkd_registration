5e53126a7bbbd43faeaa7fc344af55ad9b6c9e68
Scott Eppler <seppler@presidio.com>
2023-11-11 07:15:51 -0600

    Update module version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 module "registration_infra" {
   source  = "app.terraform.io/OKTKD/tkd-registration/kseppler"
-  version = "~>0.0.4"
+  version = "~>0.0.5"
 
   processing_queue_name           = var.processing_queue_name
   failed_registrations_queue_name = var.failed_registrations_queue_name
